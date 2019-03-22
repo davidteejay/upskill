d8e722e2f853efe991f92b82a576c540ddf12aab
Chibuokem Onyekwelu <chibuokem_tolu@hotmail.com>
2019-03-22 13:26:38 +0100

    Setup Components and Router

---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+import Nav from '../components/Nav'
+import Preloader from '../components/Preloader'
+
+export default class Home extends Component {
+	constructor(props){
+		super(props)
+
+		this.state = {
+			loading: false
+		}
+	}
+	render(){
+		return (
+			<div>
+				<Nav/>
+				<Preloader loading={this.state.loading} />
+			</div>
+		)
+	}
+}
