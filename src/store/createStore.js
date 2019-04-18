937bf3600d2bd56d9a97bc8ddca6f901da246a05
Chibuokem Onyekwelu <chibuokem_tolu@hotmail.com>
2019-04-18 18:50:54 +0100

    Bug Fix in Redux Setup

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -1,31 +1,33 @@
-import { createStore, applyMiddleware, compose, } from 'redux';
-import thunk from 'redux-thunk';
-import makeRootReducer from './reducers';
+import { createStore, applyMiddleware, compose, } from 'redux'
+import thunk from 'redux-thunk'
 import { createLogger } from 'redux-logger'
 
-const log = createLogger({ diff: true, collapsed: true });
+import makeRootReducer from './reducers'
+
+const log = createLogger({ diff: true, collapsed: true })
 
 export default (initialState = {}) => {
     //middleware configuration
-    // const middleware = { thunk, log };
+    // const middleware = { thunk, log }
 
     //store enhancers
     // const enhancers = []
 
-    const composeEnhancers = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+    const composeEnhancers = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose
 
     const enhancer = composeEnhancers(
         applyMiddleware(thunk),
+        //TODO: Remove redux logger on production
         applyMiddleware(log),
         // other store enhancers if any
-    );
+    )
 
     //store instantiation
     const store = createStore(
         makeRootReducer(),
         initialState,
         enhancer
-    );
+    )
 
-    return store;
+    return store
 }
